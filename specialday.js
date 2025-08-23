ab840f67c044acde62272f221a1a5bf81a605ee6
nhuthuynh2000 <165478116+nhuthuynh2000@users.noreply.github.com>
2025-08-23 23:53:22 +0700

    Theme

---
+++ specialday.js
@@ -0,0 +1,42 @@
+const specialDays = [
+  {
+    month: 2,
+    day: 14,
+    theme: {
+      background: "url('valentine-bg.png') no-repeat center/cover",
+      textColor: "#c1485edc",
+      heartAnimation: true,
+      fallingEmoji: ["💕", "❤️", "🌹"],
+    },
+  },
+  {
+    month: 3,
+    day: 27,
+    theme: {
+      background: "url('anniversary-bg.png') no-repeat center/cover",
+      textColor: "#c95959ff",
+      heartAnimation: true,
+      fallingEmoji: ["💑", "💘"],
+    },
+  },
+  {
+    month: 8,
+    day: 10,
+    theme: {
+      background: "url('happybirthday-em.png') no-repeat center/cover",
+      textColor: "#c95959ff",
+      heartAnimation: true,
+      fallingEmoji: ["🌺", "🎂"],
+    },
+  },
+  {
+    month: 3,
+    day: 29,
+    theme: {
+      background: "url('happybirthday-anh.png') no-repeat center/cover",
+      textColor: "#c95959ff",
+      heartAnimation: true,
+      fallingEmoji: ["🌺", "🎂"],
+    },
+  },
+];
